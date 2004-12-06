86fc7399749cb63b947df5ca9173516f4726aff6
Wincent Colaiuta <win@wincent.com>
2004-12-06 13:37:31 +0000

    Remove WinSwitchHelper.app
    git-svn-id: svn+ssh://svn.wincent.com/WinSwitch/trunk@49 03bf6e50-eddf-0310-8d46-8b7cc9a5f610

--- tag-release.sh
+++ tag-release.sh
@@ -11,15 +11,16 @@ REPOSITORY_NAME="WinSwitch"
 #
 ###
 
-TRUNK_PATH="/usr/local/svnrep/${REPOSITORY_NAME}/trunk"
-TAGS_PATH="/usr/local/svnrep/${REPOSITORY_NAME}/tags"
+PROTOCOL="http://localhost:8080"
+TRUNK_PATH="svnrep/${REPOSITORY_NAME}/trunk"
+TAGS_PATH="svnrep/${REPOSITORY_NAME}/tags"
 
 echo "The current HEAD of the main trunk will be tagged."
 echo "Enter a suitable tag [example: \"release-1.4\"]"
 read TAG
 
 /usr/local/bin/svn copy \
-  "file://${TRUNK_PATH}" "file://${TAGS_PATH}/${TAG}" \
+  "${PROTOCOL}/${TRUNK_PATH}" "${PROTOCOL}/${TAGS_PATH}/${TAG}" \
   -m "Tagging ${TAG} of ${REPOSITORY_NAME}"
 
 echo "Done"
